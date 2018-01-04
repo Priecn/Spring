514dd8459685882dfb348d7ed8a0d7aba5a5e37c
Priecn <prince.silverarrow@gmail.com>
2018-01-04 21:14:35 +0530

    Links has been created for navigation

--- shoppingCart/src/main/java/org/practice/spring/shoppingCart/controller/PageController.java
+++ shoppingCart/src/main/java/org/practice/spring/shoppingCart/controller/PageController.java
@@ -3,20 +3,35 @@ package org.practice.spring.shoppingCart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value= {"/", "/home", "/index"})
 public class PageController {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public ModelAndView index(@RequestParam(
-			name="greeting", defaultValue="Welcome to Spring web MVC!") String greeting) {
+	@RequestMapping(value= {"/", "/home", "/index"}, method=RequestMethod.GET)
+	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
+	
+	@RequestMapping(value="/about", method=RequestMethod.GET)
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value="/contact", method=RequestMethod.GET)
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contect");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+	
 	
 	
 }
