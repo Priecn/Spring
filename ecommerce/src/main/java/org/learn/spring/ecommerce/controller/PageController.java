c8aa9a45120f1b24bb23aa84982ad87e68304bff
Priecn <prince.silverarrow@gmail.com>
2018-01-20 17:25:27 +0530

    Added bootstrap theme and resolved interface and ui issues

--- ecommerce/src/main/java/org/learn/spring/ecommerce/controller/PageController.java
+++ ecommerce/src/main/java/org/learn/spring/ecommerce/controller/PageController.java
@@ -3,14 +3,37 @@ package org.learn.spring.ecommerce.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 
 public class PageController {
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String index() {
+	@RequestMapping(value= {"/", "/home"}, method=RequestMethod.GET)
+	public ModelAndView index() {
 		
-		return "page";
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+		return mv;
+	}
+	
+	@RequestMapping(value="/about", method=RequestMethod.GET)
+	public ModelAndView about() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+		
+	}
+	
+	@RequestMapping(value="/contact", method=RequestMethod.GET)
+	public ModelAndView contact() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
 		
 	}
 }
